8e27231979a9f54a42679bd3174825e08970a008
FOOPAUL <127684370+FOOPAUL@users.noreply.github.com>
2023-08-12 21:30:25 +0200

    Update Code.R

--- Code.R
+++ Code.R
@@ -1,16 +1,16 @@
-#### Load and import the data ####
+#### I. Load and import the data ####
 
 library("tidyverse")
 
 activity <- read.csv("activity.csv")
 
+activity$date <- as_date(activity$date)
 
-
-#### What is mean total number of steps taken per day? ####
-
+#### II. What is mean total number of steps taken per day? ####
+# a. the total number of steps taken per day
 stepsperday <- activity %>%
   group_by(date) %>%
-  summarize(sumstep = sum(steps, na.rm = TRUE))
+  summarize(sumstep = sum(steps))
 
 ggplot(stepsperday) +
   geom_histogram(aes(x = sumstep)) +
@@ -18,20 +18,19 @@ ggplot(stepsperday) +
        x = "Steps",
        y = "Frequency") +
   theme(plot.title = element_text(hjust = 0.5))
+ggsave("I.Total_number_of_steps_taken_each_day.pdf")
 
-# the mean of the total number of steps taken per day
-round(mean(stepsperday$sumstep))
+# b. the mean of the total number of steps taken per day
+round(mean(stepsperday$sumstep,  na.rm = T))
 
-# the median of the total number of steps taken per day
-round(median(stepsperday$sumstep))
+# c. the median of the total number of steps taken per day
+round(median(stepsperday$sumstep,  na.rm = T))
 
-### What is the average daily activity pattern? ####
-
+### III. What is the average daily activity pattern? ####
+# a. Average number of steps taken, averaged across all days
 stepperinterval <- activity %>%
   group_by(interval) %>%
   summarize(meanstep = mean(steps, na.rm = T))
-
-
 
 ggplot(stepperinterval) +
   geom_line(aes(x = interval,
@@ -40,67 +39,63 @@ ggplot(stepperinterval) +
        x = " 5-minute interval",
        y = "Average across all the days") +
   theme(plot.title = element_text(hjust = 0.5))
+ggsave("III.Average_number_of_steps_taken,_averaged_across_all_days.pdf")
 
-
+# b. maximum number of steps (5-minute interval)
 filter(stepperinterval, meanstep == max(meanstep))
 
 
-### Imputing missing values ####
+### IV. Imputing missing values ####
 
 # Calculate and report the total number of missing values in the dataset
-
+# a. total number of missing values in the dataset 
 sum(is.na(activity$steps))
 
-activitywithoutNA <- map_dbl(activity$steps, mean, na.rm = T)
-
+# b. strategy for filling in all of the missing values in the dataset
+activitywithoutNA <- map_dbl(activity$steps, mean)
 activity$steps
 
+# c. new dataset that is equal to the original dataset but with the missing data filled in.
 imputeSteps <- activity %>%
   group_by(interval) %>%
-  mutate(steps = ifelse(is.na(steps), mean(steps, na.rm = TRUE), steps))
+  mutate(steps = ifelse(is.na(steps), mean(steps), steps))
 
 
 imputedDailySteps <- imputeSteps %>%
   group_by(date) %>%
   summarise(total = sum(steps))
 
-
+# d.1. Total Daily Steps
 ggplot(imputedDailySteps, aes(total)) +
   geom_histogram(binwidth = 500) +
   labs(x = "Total Daily Steps",
        y = "Frequency",
        title = "Daily Steps") +
   theme(plot.title = element_text(hjust = 0.5))
+ggsave("IV.Daily_Steps.pdf")
 
-
-## Mean number of steps per day
+# d.2.Mean number of steps per day
 mean(imputedDailySteps$total, na.rm = TRUE)
 
-## Median number of steps per day
+# d.3. Median number of steps per day
 median(imputedDailySteps$total, na.rm = TRUE)
 
 
-### Imputing missing values ####
-# Create weekdays/weekend variable
-
-imputeSteps$date <- as.Date(imputeSteps$date)
+### V. Are there differences in activity patterns between weekdays and weekends? ####
+# a. Create weekdays/weekend variable
 
 wSteps <- imputeSteps %>%
   mutate(days = weekdays(date)) %>% 
   mutate(week = if_else(days == "samedi" | days == "dimanche","weekend", "weekday"))
 
-
-# Turn this into Factor
-
 wSteps <- wSteps %>%
   mutate(week = as.factor(week))
-
 
 wIntSteps <- wSteps %>%
   group_by(interval,week) %>%
   summarise(mean = mean(steps, na.rm = TRUE))
 
-
+# b. Weekdays and weekends activity patterns
 ggplot(wIntSteps, aes(interval, mean)) +
   geom_line() +
   facet_grid(week ~ .) +
@@ -108,6 +103,5 @@ ggplot(wIntSteps, aes(interval, mean)) +
        y = "Avarage number of steps taken")+
   ggtitle("Weekdays and weekends activity patterns")+
   theme(plot.title = element_text(hjust = 0.5))
-
-
+ggsave("V.Weekdays_and_weekends_activity_patterns.pdf")
 
